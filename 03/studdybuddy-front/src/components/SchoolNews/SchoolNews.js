32d6db0203a346712e0763297c580c87941facbb
weronikadominiak <weronika.dominiak@hotmail.com>
2021-02-05 20:00:18 +0000

    Use fetch to pull query data

--- 03/studdybuddy-front/src/components/SchoolNews/SchoolNews.js
+++ 03/studdybuddy-front/src/components/SchoolNews/SchoolNews.js
@@ -1,45 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Article, ArticleImage, Articles, Title, Wrapper } from 'components/SchoolNews/SchoolNews.styles';
 
+const  DATO_TOKEN = "e4d2a225e52a023d1d722f2dd88c33"
+const query =  ` {
+  allArticles {
+    title
+    content
+    image {
+      url
+      alt
+    }
+  }
+}
+`
 
-const SchoolNews = () => (
-  <Wrapper>
-    <Title>Gazetka szkolna</Title>
-    <Articles>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-        <h3>Wymiana komputerów</h3>
-        <p>Już niebawem w sali informatycznej 118
-          pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-    </Articles>
-  </Wrapper>
-);
+const SchoolNews = () => {
+  const [articles, setArticles] = useState();
+
+  useEffect(() => {
+    fetch('https://graphql.datocms.com', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer: ${DATO_TOKEN} `,
+      },
+      body: JSON.stringify({
+        query
+      })
+    })
+    .then(res => res.json())
+    .then(({ data: { allArticles } }) => setArticles(allArticles))
+    .catch(err => console.log(err))
+  }, [])
+  return (
+    <Wrapper>
+      <Title>Gazetka szkolna</Title>
+      <Articles>
+        {articles && articles.map(article => (
+          <Article>
+            <ArticleImage>
+              <img src={article.image.url} />
+            </ArticleImage>
+            <div>
+            <h3>{article.title}</h3>
+            <p>{article.content}</p>
+            </div>
+        </Article>
+        ))}
+      </Articles>
+    </Wrapper>
+)};
 
 SchoolNews.propTypes = {};
 
